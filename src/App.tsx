3534c0ae28b81cbf6c973ba1c95430f8a2f45a2c
Jorge-inbox <jorgehdezj.inbox@gmail.com>
2025-08-17 16:33:38 -0700

    deploy testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import { CourseInfo } from "./components/CourseInfo/CourseInfo.tsx";
 import { Courses } from "./components/Courses/components/Courses.tsx";
 import { EmptyCourseList } from "./components/EmptyCorseList/EmptyCourseList.tsx";
 import { Header } from "./components/Header/Header.tsx";
